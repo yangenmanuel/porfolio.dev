1d8dcb6b6e8d10905c2c4dcd213557abd63248b9
Yang Enmanuel <enmanuelcalzat@gmail.com>
2024-02-09 10:55:58 -0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 
 import robotsTxt from "astro-robots-txt"
 
@@ -8,7 +8,7 @@ import robotsTxt from "astro-robots-txt"
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://porfolio.dev/',
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
